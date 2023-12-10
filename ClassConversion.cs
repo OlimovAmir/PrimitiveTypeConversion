c2eb94749ade9e9286f40606e4bd90152ec96865
olimov Amir <olimov.amir@mail.ru>
2023-12-10 12:26:21 +0300

    Добавьте файлы проекта.

---
+++ ClassConversion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimitiveTypeConversion
+{
+    public class ClassConversion
+    {
+        public static void Foo(float value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 21;
+
+        PrimitiveTypeConversion.ClassConversion classConversion = new PrimitiveTypeConversion.ClassConversion();
+        PrimitiveTypeConversion.ClassConversion.Foo(a);
+    }
+}
